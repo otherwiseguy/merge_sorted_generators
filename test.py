e510697093c702766252c8b468a4655d46cbf597
Terry Wilson <twilson@redhat.com>
2020-05-26 23:20:00 -0500

    Add merge_union

--- test.py
+++ test.py
@@ -26,8 +26,37 @@ def merge_unique(*sorted_gen):
         yield val
 
 
-a = iter(range(0, 5))
+def merge_union(*sorted_gens):
+    cut_off = None
+    while True:
+        for gen in sorted_gens:
+            if cut_off is None:
+                try:
+                    cut_off = next(gen)
+                    matches = 1
+                except StopIteration:
+                    return # Can't match any of the rest if one empty
+            else:
+                while True:
+                    try:
+                        val = next(gen)
+                    except StopIteration:
+                        return
+                    if val >= cut_off:
+                        break
+                if  val != cut_off:
+                    cut_off = val
+                    matches = 1
+                else:
+                    matches += 1
+            if matches == len(sorted_gens):
+                yield val
+
+
+a = iter(range(0, 6))
 b = iter(range(4, 7))
-c = iter(range(3, 8))
-print(list(merge_unique(a, b, c)))
-print(list(merge_unique(iter([]))))
+c = iter(range(5, 6))
+print(list(merge_union(a, b, c)))
+#print(list(merge_unique(a, b, c)))
+#print(list(merge_unique(iter([]))))
+#print(list(merge_unique(iter([1, 2]))))
